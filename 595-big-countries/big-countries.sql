384b71aa53d6c50d11944fb304b59ab787cf2b8f
P N V HARI SURYA PRAKASH REDDY <153354030+pnvharisuryaprakashreddy@users.noreply.github.com>
2026-06-24 14:29:39 +0530

    Time: 271 ms (99.73%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select name, area, population
 from World 
-where area >= 3000000 or population >= 25000000;
+where population >= 25000000 or area >= 3000000;
